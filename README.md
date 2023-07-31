9d1c90ac11f1e2f08a508e28cb99c1c5187988ed
anastasia-tea <anastasia.chapinfortin@gmail.com>
2023-07-31 16:56:57 -0500

    Added choosing operation, decimal point input, working on operate and display result

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ Inspired by:
 References:
 -https://codingbeautydev.com/blog/javascript-round-number-to-2-decimal-places/#:~:text=December%2011%2C%202022-,To%20round%20a%20number%20to%202%20decimal%20places%20in%20JavaScript,number%20to%202%20decimal%20places
 -https://stackoverflow.com/questions/27946703/javascript-foreach-add-addeventlistener-on-all-buttons
+-https://www.freecodecamp.org/news/how-to-convert-a-string-to-a-number-in-javascript/
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 const digits = document.querySelectorAll(".digit");
-const operations = document.querySelectorAll(".operations");
+const operations = document.querySelectorAll(".operation");
 const point = document.querySelector("#point");
 const equals = document.querySelector("#equals");
 
@@ -11,17 +11,28 @@ const screen = document.querySelector("#screen");
 
 //
 
+point.addEventListener("click", inputPoint);
+equals.addEventListener("click", display);
+//del.addEventListener("click");
 clear.addEventListener("click", clearScreen);
 
 //
 
-let firstOperand = "";
-let secondOperand = "";
-let current = "";
+let operand1 = "";
+let operand2 = "";
+let operation = "";
+
+//
 
 digits.forEach(function(digit) {
     digit.addEventListener("click", function() {
         inputDigit(digit.textContent);
+    })
+});
+
+operations.forEach(function(op) {
+    op.addEventListener("click", function() {
+        inputOperation(op.textContent);
     })
 });
 
@@ -34,9 +45,34 @@ function inputDigit(digit) {
     screen.textContent += digit;
 }
 
+function inputOperation(op) {
+    if (operation === "") {
+        operation = op;
+        screen.textContent += op;
+    }
+}
+
+function inputPoint() {
+    if (!screen.textContent.includes(".")) {
+        if (screen.textContent === "") {
+            screen.textContent = "0";
+        }
+        screen.textContent += ".";
+    }
+}
+
+function backspace() {
+
+}
+
 function clearScreen() {
     screen.textContent = "";
+    operand1 = "";
+    operand2 = "";
+    operation = "";
 }
+
+//
 
 function add(x, y) {
     let result = round(x + y);
@@ -61,6 +97,23 @@ function divide(x, y) {
     return result;
 }
 
-function operate() {
+function operate(op, x, y) {
+    x = parseFloat(x);
+    y = parseFloat(y);
+    
+    if (op === "+") {
+        return add(x, y);
+    } else if (op === "-") {
+        return subtract(x, y);
+    } else if (op === "*") {
+        return multiply(x, y);
+    } else if (op === "÷") {
+        return divide(x, y);
+    } else {
+        return "Error!";
+    }
+}
+
+function display() {
 
 }
