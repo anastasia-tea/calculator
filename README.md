68fce0a4e59b0f5242bca0aa925d6f1339223917
anastasia-tea <anastasia.chapinfortin@gmail.com>
2023-07-31 15:41:03 -0500

    Fixed formatting, added functions for rounding results, adding digit input, clearing screen

--- README.md
+++ README.md
@@ -4,5 +4,9 @@ This is the Odin Project Foundation's final project, the Calculator!
 
 After starting and restarting this program a number of times during my CS degree, it feels very meaningful to see how far I've come!
 
+Inspired by:
+-https://www.alibaba.com/product-detail/colorful-keys-solar-desktop-calculator-with_60185160895.html
+
 References:
--
+-https://codingbeautydev.com/blog/javascript-round-number-to-2-decimal-places/#:~:text=December%2011%2C%202022-,To%20round%20a%20number%20to%202%20decimal%20places%20in%20JavaScript,number%20to%202%20decimal%20places
+-https://stackoverflow.com/questions/27946703/javascript-foreach-add-addeventlistener-on-all-buttons
--- script.js
+++ script.js
@@ -1,23 +1,66 @@
 
+const digits = document.querySelectorAll(".digit");
+const operations = document.querySelectorAll(".operations");
+const point = document.querySelector("#point");
+const equals = document.querySelector("#equals");
+
+const del = document.querySelector("#delete");
+const clear = document.querySelector("#clear");
+
+const screen = document.querySelector("#screen");
+
+//
+
+clear.addEventListener("click", clearScreen);
+
+//
+
+let firstOperand = "";
+let secondOperand = "";
+let current = "";
+
+digits.forEach(function(digit) {
+    digit.addEventListener("click", function() {
+        inputDigit(digit.textContent);
+    })
+});
+
+function round(number) {
+    let result = number.toFixed(4);
+    return result;
+}
+
+function inputDigit(digit) {
+    screen.textContent += digit;
+}
+
+function clearScreen() {
+    screen.textContent = "";
+}
+
 function add(x, y) {
-    return (x + y);
+    let result = round(x + y);
+    return result;
 }
 
 function subtract(x, y) {
-    return (x - y);
+    let result = round(x - y);
+    return result;
 }
 
 function multiply(x, y) {
-    return (x * y);
+    let result = round(x * y);
+    return result;
 }
 
 function divide(x, y) {
     if (y == 0) {
         return "Error!";
     }
-    return (x / y);
+    let result = round(x / y);
+    return result;
 }
 
 function operate() {
-    
+
 }
